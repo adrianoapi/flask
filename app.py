2e85aecf08bbc613b08ba1e1b8022cbc97eff696
Adriano API <adrianoapi@hotmail.com>
2018-07-16 15:28:28 -0300

    print response json

--- app.py
+++ app.py
@@ -4,5 +4,18 @@ app = Flask(__name__, static_folder = "public")
 @app.route('/<name>', methods = ['GET','POST', 'PUT'])    
  
 def hello(name):
-    return jsonify(name = name)
+    return jsonify( name    = name,
+                    email   = ['sdcomputadores@gmail.com','adrianoapi@hotmail.com',
+                        {
+                            'name'   : 'Adriano A Costa',
+                            'company': 'Microsoft'
+                        }
+                    ],
+                    
+                    addres  = {
+                        'street': "Rua Marambai",
+                        'number': 158,
+                        'city'  : "Sao Paulo",
+                        'state' : "SP"
+                    })
 
