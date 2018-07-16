b993669081c617fe23d9a0b548fd57528ab9fd30
Adriano API <adrianoapi@hotmail.com>
2018-07-16 16:38:34 -0300

    add coneccion and user model

--- app.py
+++ app.py
@@ -1,6 +1,21 @@
 from flask import Flask, request, jsonify, render_template, abort, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder = "public")
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:@localhost/flask"
+db = SQLAlchemy(app)
+
+# User model
+class User(db.Model):
+    id       = db.Column(db.Integer, primary_key = True)
+    username = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    
+    def __init__(self, username, password):
+        slef.username = username
+        self.password = password
+        
 
 @app.route('/<name>', methods = ['GET','POST', 'PUT'])    
 def hello(name):
