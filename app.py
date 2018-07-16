791c9796f2305dcb9088378ca647217d46ff0061
Adriano API <adrianoapi@hotmail.com>
2018-07-16 16:00:55 -0300

    add redirect

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
-from flask import Flask, request, jsonify, render_template, abort, redirect
+from flask import Flask, request, jsonify, render_template, abort, redirect, url_for
 
 app = Flask(__name__, static_folder = "public")
 
 @app.route('/<name>', methods = ['GET','POST', 'PUT'])    
 def hello(name):
     abort(404)
-    return render_template('hello.html', name = name)
+    return redirect(url_for('/'))
+    #return render_template('hello.html', name = name)
+
+@app.route('/')
+def root():
+    return "index"
 
 @app.errorhandler(404)
 def not_found(error):
