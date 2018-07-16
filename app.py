cb9d1f416aa226c71465c22c86710ba6488ab8e1
Adriano API <adrianoapi@hotmail.com>
2018-07-16 15:56:19 -0300

    add decorator error404

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, abort, redirect
+
 app = Flask(__name__, static_folder = "public")
 
 @app.route('/<name>', methods = ['GET','POST', 'PUT'])    
- 
 def hello(name):
+    abort(404)
     return render_template('hello.html', name = name)
+
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
